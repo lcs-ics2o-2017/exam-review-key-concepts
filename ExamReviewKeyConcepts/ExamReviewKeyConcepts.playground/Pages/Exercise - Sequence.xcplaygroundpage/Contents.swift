437ee022d5b5484408831fd21434bdda04f94b2f
Russell Gordon <rgordon@lcs-mba2410.lcs.on.ca>
2018-04-27 11:42:07 -0400

    Fixed border width bug at high res canvas quality, added colour constants, and two images (grid and no grid)

--- ExamReviewKeyConcepts/ExamReviewKeyConcepts.playground/Pages/Exercise - Sequence.xcplaygroundpage/Contents.swift
+++ ExamReviewKeyConcepts/ExamReviewKeyConcepts.playground/Pages/Exercise - Sequence.xcplaygroundpage/Contents.swift
@@ -5,12 +5,13 @@ import Cocoa
 import PlaygroundSupport
 /*:
  ## Exercise
-
+ 
  Each image is exactly 400 pixels wide by 600 pixels tall.
  
  Your goal is to precisely reproduce this image (the underlying image, not the planning grid):
  
- ![tjamc](tjamc.png "The Jesus and Mary Chain")
+ ![tjamc-no-grid.png](tjamc-no-grid.png "The Jesus and Mary Chain")
+ ![tjamc-with-grid.png](tjamc-with-grid.png "The Jesus and Mary Chain")
  
  By precisely reproducing this image, you will demonstrate your understanding of:
  
@@ -20,9 +21,16 @@ import PlaygroundSupport
  
  You [can use this site](http://www.workwithcolor.com/color-converter-01.htm) to convert colours to HSB to use with the Canvas class.
  
+ Or, if you wish, you may use the color constants created below.
  */
+
 // Create a new canvas
 let canvas = Canvas(width: 400, height: 600)
+
+// COLORS
+let grey = Color(hue: 219, saturation: 1, brightness: 82, alpha: 100)
+let lightBlue = Color(hue: 205, saturation: 74, brightness: 77, alpha: 75)
+let lightRed = Color(hue: 4, saturation: 78, brightness: 92, alpha: 100)
 
 // HINT: Remember that all shapes have a fill and a border.
 //       You can turn off the fill or border if desired.
@@ -37,9 +45,9 @@ canvas.drawShapesWithBorders = true
 canvas.drawShapesWithFill = false
 canvas.drawEllipse(centreX: 200, centreY: 100, width: 50, height: 75)
 
-
 /*:
  ## Template code
  The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
  */
 PlaygroundPage.current.liveView = canvas.imageView
+
--- ExamReviewKeyConcepts/ExamReviewKeyConcepts.playground/Pages/Exercise - Sequence.xcplaygroundpage/Sources/Helper.swift
+++ ExamReviewKeyConcepts/ExamReviewKeyConcepts.playground/Pages/Exercise - Sequence.xcplaygroundpage/Sources/Helper.swift
@@ -359,49 +359,49 @@ open class Canvas : CustomPlaygroundQuickLookable {
         
     }
     
-//    // Draw text on the image
-//    open func drawText(message: String, size: Int, x: Int, y: Int, kerning : Float)  {
-//        
-//        // Set attributes of shape based on the canvas scale factor
-//        var size = size
-//        size *= scale
-//        var x = x
-//        x *= scale
-//        var y = y
-//        y *= scale
-//        var kerning = kerning
-//        kerning *= Float(scale)
-//        
-//        // Convert the provided String object to an NSString object
-//        let string: NSString = NSString(string: message)
-//        
-//        // set the text color to dark gray
-//        let fieldColor : NSColor = NSColor(hue: textColor.translatedHue, saturation: textColor.translatedSaturation, brightness: textColor.translatedBrightness, alpha: textColor.translatedAlpha)
-//        
-//        // set the font to Helvetica Bold 24
-//        //        let fieldFont = NSFont(name: "Berthold Akzidenz Grotesk Medium", size: CGFloat(size))
-//        let fieldFont = NSFont(name: "Helvetica Bold", size: CGFloat(size))
-//        
-//        // set the line spacing to 1
-//        let paraStyle = NSMutableParagraphStyle()
-//        paraStyle.lineSpacing = 1.0
-//        
-//        // set the Obliqueness (tilt of text) to 0.0
-//        let skew = 0.0
-//        
-//        // create dictionary with attributes of the string to be drawn
-//        let attributes: [String : AnyObject] = [
-//            NSForegroundColorAttributeName: fieldColor,
-//            NSParagraphStyleAttributeName: paraStyle,
-//            NSObliquenessAttributeName: skew as AnyObject,
-//            NSFontAttributeName: fieldFont!,
-//            NSKernAttributeName: NSNumber(value: kerning) as AnyObject
-//        ]
-//        
-//        // Draw the string
-//        string.draw(at: NSPoint(x: x, y: y), withAttributes: attributes)
-//        
-//    }
+    //    // Draw text on the image
+    //    open func drawText(message: String, size: Int, x: Int, y: Int, kerning : Float)  {
+    //
+    //        // Set attributes of shape based on the canvas scale factor
+    //        var size = size
+    //        size *= scale
+    //        var x = x
+    //        x *= scale
+    //        var y = y
+    //        y *= scale
+    //        var kerning = kerning
+    //        kerning *= Float(scale)
+    //
+    //        // Convert the provided String object to an NSString object
+    //        let string: NSString = NSString(string: message)
+    //
+    //        // set the text color to dark gray
+    //        let fieldColor : NSColor = NSColor(hue: textColor.translatedHue, saturation: textColor.translatedSaturation, brightness: textColor.translatedBrightness, alpha: textColor.translatedAlpha)
+    //
+    //        // set the font to Helvetica Bold 24
+    //        //        let fieldFont = NSFont(name: "Berthold Akzidenz Grotesk Medium", size: CGFloat(size))
+    //        let fieldFont = NSFont(name: "Helvetica Bold", size: CGFloat(size))
+    //
+    //        // set the line spacing to 1
+    //        let paraStyle = NSMutableParagraphStyle()
+    //        paraStyle.lineSpacing = 1.0
+    //
+    //        // set the Obliqueness (tilt of text) to 0.0
+    //        let skew = 0.0
+    //
+    //        // create dictionary with attributes of the string to be drawn
+    //        let attributes: [String : AnyObject] = [
+    //            NSForegroundColorAttributeName: fieldColor,
+    //            NSParagraphStyleAttributeName: paraStyle,
+    //            NSObliquenessAttributeName: skew as AnyObject,
+    //            NSFontAttributeName: fieldFont!,
+    //            NSKernAttributeName: NSNumber(value: kerning) as AnyObject
+    //        ]
+    //
+    //        // Draw the string
+    //        string.draw(at: NSPoint(x: x, y: y), withAttributes: attributes)
+    //
+    //    }
     
     // Draw text on the image
     open func drawText(message: String, size: Int, x: Int, y: Int)  {
@@ -465,12 +465,8 @@ open class Canvas : CustomPlaygroundQuickLookable {
         // Make the new path
         let path = NSBezierPath(ovalIn: NSRect(x: centreX - width/2, y: centreY - height/2, width: width, height: height))
         
-        // Set width of border
-        if borderWidth > 0 {
-            path.lineWidth = CGFloat(borderWidth)
-        } else {
-            path.lineWidth = CGFloat(self.defaultBorderWidth * scale)
-        }
+        // BUG FIX: Border width for higher quality rendering
+        path.lineWidth = CGFloat(self.defaultBorderWidth)
         
         // Set ellipse border color
         NSColor(hue: borderColor.translatedHue, saturation: borderColor.translatedSaturation, brightness: borderColor.translatedBrightness, alpha: borderColor.translatedAlpha).setStroke()
