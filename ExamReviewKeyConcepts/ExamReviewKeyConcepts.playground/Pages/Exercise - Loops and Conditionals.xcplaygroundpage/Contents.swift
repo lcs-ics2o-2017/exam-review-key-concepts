04b90372a6531b16406ea96a28a1808f4e3f4dbf
Russell Gordon <rgordon@lcs-mba2410.lcs.on.ca>
2018-04-27 12:27:57 -0400

    Revised Sloan practice page to include image with grid and without.
    Also added colour constants.

--- ExamReviewKeyConcepts/ExamReviewKeyConcepts.playground/Pages/Exercise - Loops and Conditionals.xcplaygroundpage/Contents.swift
+++ ExamReviewKeyConcepts/ExamReviewKeyConcepts.playground/Pages/Exercise - Loops and Conditionals.xcplaygroundpage/Contents.swift
@@ -1,4 +1,4 @@
-//: [Previous](@previous) / [Next](@next)
+//: [Previous](@previous)
 //: # Exercise - Loops and Conditionals
 //: The following two statements are required to make the playground run. Please do not remove.
 import Cocoa
@@ -10,7 +10,8 @@ import PlaygroundSupport
  
  Your goal is to precisely reproduce this image (the underlying image, not the planning grid):
  
- ![sloan](sloan.png "Sloan")
+ ![sloan-no-grid.png](sloan-no-grid.png "Sloan")
+ ![sloan-with-grid.png](sloan-with-grid.png "Sloan")
  
  By precisely reproducing this image, you will demonstrate your understanding of:
  
@@ -22,10 +23,17 @@ import PlaygroundSupport
  
  You [can use this site](http://www.workwithcolor.com/color-converter-01.htm) to convert colours to HSB to use with the Canvas class.
  
+ Or, if you wish, you may use the color constants created below.
+ 
  */
 
 // Create a new canvas
 let canvas = Canvas(width: 400, height: 600)
+
+// COLOR CONSTANTS
+let deepOrange = Color(hue: 12, saturation: 78, brightness: 92, alpha: 100)
+let lightBlue = Color(hue: 195, saturation: 63, brightness: 92, alpha: 100)
+let neonGreen = Color(hue: 107, saturation: 61, brightness: 93, alpha: 100)
 
 // HINT: Remember that all shapes have a fill and a border.
 //       You can turn off the fill or border if desired.
